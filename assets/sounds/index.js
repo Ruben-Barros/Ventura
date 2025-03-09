ced3bf883bdd31ad2b38bf8f3cfbd9cd23e38e0e
Ruben-Barros <ruben.barros.dev@gmail.com>
2025-03-09 12:09:57 +0000

    Add sound assets index file for better asset management

---
+++ assets/sounds/index.js
@@ -0,0 +1,17 @@
+// This file exports all sound assets for easier importing
+export default {
+  // UI Sounds
+  introChime: require('./intro-chime.mp3'),
+  choiceBell: require('./choice-bell.mp3'),
+  choiceSelect: require('./choice-select.mp3'),
+  
+  // Ambient Sounds
+  forestAmbiance: require('./forest-ambiance.mp3'),
+  caveAmbiance: require('./cave-ambiance.mp3'),
+  villageAmbiance: require('./village-ambiance.mp3'),
+  
+  // Effects
+  footsteps: require('./footsteps.mp3'),
+  doorOpen: require('./door-open.mp3'),
+  magicSpell: require('./magic-spell.mp3'),
+}; 
